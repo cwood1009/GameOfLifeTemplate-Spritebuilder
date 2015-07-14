b9e4f648c96d33be81b42100298e51ed7f4fd605
Chris Wood <cwood@Chriss-MBP.domain>
2015-07-13 23:14:42 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -10,7 +10,7 @@
 #import "Creature.h"
 
 static const int GRID_ROWS = 8;
-static const int GRID_COLUMNS = 8;
+static const int GRID_COLUMNS = 10;
 
 @implementation Grid {
     NSMutableArray *_gridArray;
