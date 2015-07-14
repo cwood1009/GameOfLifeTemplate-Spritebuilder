1389748b6d21fa93c99f9dee72c9dd55fbdf13f4
Chris Wood <cwood@Chriss-MBP.domain>
2015-07-14 11:20:29 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -148,12 +148,16 @@ static const int GRID_COLUMNS = 10;
         for (int j = 0; j < [_gridArray[i]count]; j++) {
             Creature *currentCreature = _gridArray[i][j];
             
+            
             if (currentCreature.livingNeighbors == 3) {
                 currentCreature.isAlive = true;
-                numAlive++;
+                
                 
             }else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4) {
                 currentCreature.isAlive = false;
+            }
+            if (currentCreature.isAlive == true) {
+                numAlive++;
             }
         }
     }
